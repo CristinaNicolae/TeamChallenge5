0268338476c99e43ba8f0b978db875fc835e4bee
CiprianMT <75330126+ctapu@users.noreply.github.com>
2020-12-16 21:12:20 +0200

    Add files via upload

---
+++ app/src/main/java/com/scoalainformala/teamchallenge5/MainViewModel.java
@@ -0,0 +1,16 @@
+package com.wikitech.myapplication;
+
+import android.util.Log;
+
+import androidx.lifecycle.ViewModel;
+
+public class MainViewModel extends ViewModel {
+
+    public void onAddButtonHit() {
+
+    }
+
+    public void onSearchButtonHit() {
+
+    }
+}
